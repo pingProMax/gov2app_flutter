57e7a3277bfdfc02186fd706a689f3b8a13331d8
pings <ssrpub@gmail.com>
2023-10-26 19:44:01 +0800

    up

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -55,7 +55,7 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    if(_userName != "" && _password != ""){
+    if(PrefUtil.preferences.getString("userName") != null && PrefUtil.preferences.getString("password") != null){
       loginHandle();
     }
 
